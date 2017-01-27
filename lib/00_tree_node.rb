ed5d7ba719523c508e355a9a6913ba30cb9019a5
App Academy Student <student@appacademy.io>
2017-01-27 14:50:33 -0800

    Add dfs - 4 fails

--- lib/00_tree_node.rb
+++ lib/00_tree_node.rb
@@ -1,3 +1,5 @@
+require 'byebug'
+
 class PolyTreeNode
   def initialize(value)
     @value = value
@@ -45,4 +47,37 @@ class PolyTreeNode
   def inspect
     @value
   end
+
+# Write a #dfs(target_value) method which takes a value
+# to search for and performs the search. Write this recursively.
+# First, check the value at this node. If a node's value matches
+# the target value, return the node.
+# If not, iterate through the #children and repeat.
+  def dfs(target_value)
+    p self
+    return self if self.value == target_value
+
+    self.children.each do |child|
+      success = child.dfs(target_value)
+      return success if success
+    end
+    nil
+  end
+
 end
+
+# a = PolyTreeNode.new("a")
+# b = PolyTreeNode.new("b")
+# c = PolyTreeNode.new("c")
+# d = PolyTreeNode.new("d")
+# e = PolyTreeNode.new("e")
+# f = PolyTreeNode.new("f")
+# g = PolyTreeNode.new("g")
+# b.parent = a
+# c.parent = a
+# d.parent = a
+# e.parent = b
+# f.parent = b
+# g.parent = b
+#
+# a.dfs("f")
